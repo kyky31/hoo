75cd436cce6cc3520310bd6b0621011ac9dff44f
kyky31 <129773783+kyky31@users.noreply.github.com>
2023-10-27 17:49:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+con = sqlite3.connect('school.sqlite')
+cur = con.cursor()
+
+que_create = '''
+INSERT INTO class (name, surname, mark) VALUES
+    ('Василий', 'Пупкин', 3),
+    ('Денис', 'Синицан', 4),
+    ('Ангелина', 'Соколова', 5),
+    ('Саша', 'Петров', 2)
+'''
+
+cur.execute(que_create)
+con.commit()
+
+con.close()
+
+
